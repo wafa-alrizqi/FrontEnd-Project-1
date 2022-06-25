cbb777096eade31e25d318ac55630b0d58011965
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-06-26 00:00:37 +0300

    added sign in page and updated style

--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,44 @@ function myFunction() {
 
     }
 }
-var contentDIV =document.getElementById("contentDIV")
-contentDIV=window.location.replace()
+var fact_f =document.getElementById("fact-f")
+fact_f=window.location.replace()
 
 
 function random_fact(){
     random1 = Math.ceil(Math.random()*6)
-    var span =document.getElementById("fact-f")
+    // var span =document.getElementById("fact-f")
+    var h4 = document.getElementById("h4-fact")
+    var p = document.getElementById("p-fact")
+    var img = document.getElementById("img-fact")
+
+    
+
 
     if (random1 == 1){
-        span.innerText='fact1'
+        h4.innerText=' Sudan has more pyramids than any country in the world'
+        p.innerText=
+        "Not only does Sudan have more pyramids than Egypt, but the numbers aren't even close. While 138 pyramids have been discovered in Egypt, Sudan boasts around 255. Next, see if you can answer these real Jeopardy! questions about geography."
+        img.src="https://www.rd.com/wp-content/uploads/2021/05/sudan-pyramids-interesting-fact_rd.com-2-Getty-Images.jpg?resize=768,512"
+
     }
     else if (random1 == 2){
-        span.innerText='fact2'
+        h4.innerText='fact2'
+        p.innerText=
+        "Not only"
+        img.src="https://www.rd.com/wp-content/uploads/2021/05/sudan-pyramids-interesting-fact_rd.com-2-Getty-Images.jpg?resize=768,512"
         }
     else if (random1 == 3){
-        span.innerText='fact3'
+        h4.innerText='fact3'
         }
     else if (random1 == 4) { 
-        span.innerText='fact4'
+        h4.innerText='fact4'
     }
     else if (random1 == 5) {
-        span.innerText='fact5'
+        h4.innerText='fact5'
         }
     else {
-        span.innerText='fact6'
+        h4.innerText='fact6'
     } 
 
 }
